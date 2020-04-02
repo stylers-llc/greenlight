b20f6a7b9f962744c8534d1bceeed9c897a6550f
Gabor Nagy <gabor.nagy@stylersonline.com>
2020-04-02 19:56:39 +0200

    copy change

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@
 Rails.application.routes.draw do
   get '/health_check', to: 'health_check#all'
 
+  # ASZF
+  get '/aszf', to: 'main#aszf', as: :aszf
+
   # Error routes.
   match '/401', to: 'errors#unauthorized', via: :all, as: :unauthorized
   match '/404', to: 'errors#not_found', via: :all, as: :not_found
